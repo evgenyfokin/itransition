d531e5d348218683190e12396f1dff4eaf7ef2a4
evgenyfokin <fokin_e@inbox.ru>
2023-06-09 20:54:57 +0300

    compleated task 5

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {makeServer} from "./server";
+
+if (process.env.NODE_ENV === 'development') {
+    makeServer();
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
